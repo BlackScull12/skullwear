85607d935f08bc0424d10ab55ee46b31fdce719f
BlackScull12 <87164998+BlackScull12@users.noreply.github.com>
2026-02-22 17:49:31 +0530

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,14 +1,23 @@
 fetch("data/products.json")
-.then(res => res.json())
-.then(data => {
-  const container = document.getElementById("products");
-  data.forEach(p => {
-    container.innerHTML += `
-      <div class="card">
-        <img src="${p.image}">
-        <h3>${p.name}</h3>
-        <p>${p.price} ${p.currency}</p>
-        <a href="checkout.html?item=${p.id}">BUY</a>
-      </div>`;
+  .then(res => {
+    if (!res.ok) throw new Error("products.json not found");
+    return res.json();
+  })
+  .then(data => {
+    const container = document.getElementById("products");
+    if (!container) throw new Error("Missing #products div");
+
+    data.forEach(p => {
+      container.innerHTML += `
+        <div class="card">
+          <img src="${p.image}">
+          <h3>${p.name}</h3>
+          <p>${p.price} ${p.currency}</p>
+          <a href="checkout.html">BUY</a>
+        </div>`;
+    });
+  })
+  .catch(err => {
+    console.error(err);
+    document.body.innerHTML += "<p style='color:red'>Store failed to load</p>";
   });
-});
